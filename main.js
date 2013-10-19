77aea2721f3cb0c007c634f2745c31aa17bf1cf0
s1022577 <t2528331@gmail.com>
2013-10-19 14:45:02 +0800

    add main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+(function () {
+var canvas= document.getElementById('game'),
+    ctx= canvas.getContext('2d'),
+    ballX= Math.floor(Math.random() *300),//0...300
+    ballY= Math.floor(Math.random() *500),
+    ballZ= Math.floor(Math.random() *100);
+
+
+canvas.width= 480;
+canvas.height= 320;
+
+ctx.fillstyle='black';
+ctx.beginPath();
+ctx.arc(ballX, ballY, ballZ,0, Math.PI *2, true);
+ctx.fill();
+
+}) ();
