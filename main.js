2c82f08a380925ce242dd13e82e34196378beac6
s1022577 <t2528331@gmail.com>
2013-10-19 16:49:24 +0800

    fix #9

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ var gameModule = (function () {
 	function start(){
 		var canvas= document.getElementById('game'),
 		    ctx= canvas.getContext('2d'),
-		    ballX= Math.floor(Math.random() *300),//0...300
-		    ballY= Math.floor(Math.random() *500),
-		    ballZ= Math.floor(Math.random() *100);
+		    ballX= Math.floor(Math.random() *600),//0...300
+		    ballY= Math.floor(Math.random() *450),
+		    ballZ= Math.floor(Math.random() *80);
 
-		canvas.width= 480;
-		canvas.height= 320;
+		canvas.width= 640;
+		canvas.height= 480;
 
-		ctx.fillstyle='black';
+		ctx.fillStyle='Red';
 		ctx.beginPath();
 		ctx.arc(ballX, ballY, ballZ,0, Math.PI *2, true);
 		ctx.fill();
