216cb0177200d33f1dfe096f433898209d7c0788
s1022577 <t2528331@gmail.com>
2013-10-19 15:13:56 +0800

    fix #7

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-(function () {
-var canvas= document.getElementById('game'),
-    ctx= canvas.getContext('2d'),
-    ballX= Math.floor(Math.random() *300),//0...300
-    ballY= Math.floor(Math.random() *500),
-    ballZ= Math.floor(Math.random() *100);
+var gameModule = (function () {
+	function start(){
+		var canvas= document.getElementById('game'),
+		ctx= canvas.getContext('2d'),
+		ballX= Math.floor(Math.random() *300),//0...300
+		ballY= Math.floor(Math.random() *500),
+		ballZ= Math.floor(Math.random() *100);
 
-canvas.width= 480;
-canvas.height= 320;
+		canvas.width= 480;
+		canvas.height= 320;
 
-ctx.fillstyle='black';
-ctx.beginPath();
-ctx.arc(ballX, ballY, ballZ,0, Math.PI *2, true);
-ctx.fill();
-
+		ctx.fillstyle='black';
+		ctx.beginPath();
+		ctx.arc(ballX, ballY, ballZ,0, Math.PI *2, true);
+		ctx.fill();
+}
+    return{
+    	start:start
+    }		
 }) (); 
+
+gameModule.start();
